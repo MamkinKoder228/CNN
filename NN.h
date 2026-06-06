a759e06b5db37c8a571d37f4478b8072cb2a86b5
Ares <zayats.arsenii@gmail.com>
2026-06-06 14:41:28 +0300

    Added LReLU and AF derivatives

--- NN.h
+++ NN.h
@@ -6,21 +6,38 @@
 	typedef double Real;
 #endif
 
-typedef void (*ActivationFunction)(Real*, Real*, size_t n);
+typedef void (*ActivationFunction)(Real *, Real *, size_t n);
+typedef void (*ActivationDerivative)(Real *, Real *, size_t n);
 
 typedef struct {
 	size_t XDim, YDim;
 	Real *W, *B;
 	ActivationFunction Activation;
+	ActivationDerivative Derivative;
 } Layer;
 
-void Feed(Layer *L, Real *X, Real *Destination);
+void LReLU(Real *X, Real *Y, size_t n);
+void DLReLU(Real *X, Real *Y, size_t n);
+void Feed(Layer *L, Real *X, Real *Y);
 
-void Feed(Layer *L, Real *X, Real *Destination){
-	for (int i = 0; i < L->YDim; ++i){
-		Destination[i] = L->B[i];
-		for (int j = 0; j < L->XDim; ++j){
-			Destination[i] += X[j] * L->W[j + i * N->XDim];
+void LReLU(Real *X, Real *Y, size_t n){
+	for (size_t i = 0; i < n; ++i){
+		Y[i] = X[i] < 0? X[i] * 0.1 : X[i];
+	}
+}
+
+void DLReLU(Real *X, Real *Y, size_t n){
+	for (size_t i = 0; i < n; ++i){
+		Y[i] = X[i] < 0? 0.1 : 1;
+	}
+}
+
+void Feed(Layer *L, Real *X, Real *Y){
+	for (size_t i = 0; i < L->YDim; ++i){
+		Y[i] = L->B[i];
+		for (size_t j = 0; j < L->XDim; ++j){
+			Y[i] += X[j] * L->W[j + i * L->XDim];
 		}
 	}
+	L->Activation(Y, Y, L->YDim);
 }
--- test.c
+++ test.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include "NN.h"
 
 int main(void){
+	double W1[2][2] = 
+	{
+		{1, -1},
+		{1, -1}
+	};
+
+	double W2[1][2] = 
+	{
+		{1, 1},
+	};	
+
+	Real B[2] = {0, 0};
+
+	Real X[2] = {1, 0};
+	Real Y[2];
+	Layer L1 = {2, 2, W1, B, LReLU};
+	Layer L2 = {2, 1, W2, B, LReLU};
+	Feed(&L1, X, Y);
+	Feed(&L2, Y, Y);
+	printf("%.3f\n", Y[0]);
+
 	return 0;
 }
